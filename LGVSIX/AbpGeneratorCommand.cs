867a92b83d8494ef626d8b65a294f0a49922f9ab
wang-jie-2020 <421004034@qq.com>
2020-09-30 11:06:04 +0800

    修改框架版本，增加插件（有问题）

---
+++ LGVSIX/AbpGeneratorCommand.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LG.AbpDtoGenerator;
+using LG.AbpDtoGenerator.CodeAnalysis;
+using LG.AbpDtoGenerator.Models;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Package;
+using Microsoft.VisualStudio.Shell;
+using Newtonsoft.Json;
+using Task = System.Threading.Tasks.Task;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.Globalization;
+using DtoGenerator.Vsix;
+using Document = Microsoft.CodeAnalysis.Document;
+using Process = System.Diagnostics.Process;
+using Project = Microsoft.CodeAnalysis.Project;
+
+namespace LGVSIX
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class AbpGeneratorCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("da72badb-d4e5-42b8-99f7-d12df3bb7cd8");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AbpGeneratorCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private AbpGeneratorCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static AbpGeneratorCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in AbpGeneratorCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new AbpGeneratorCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+                Workspace workspace = ((IComponentModel)Package.GetGlobalService(typeof(SComponentModel))).GetService<Workspace>();
+
+                ProjectItem selectedItem = this.GetSelectedSolutionExplorerItem();
+                if (selectedItem != null && selectedItem.Name != null && !selectedItem.Name.EndsWith(".cs"))
+                {
+                    "52ABP代码生成器(官网：52abp.com),仅支持C#文件！".ErrorMsg();
+                }
+                else
+                {
+                    Document currentSelectedDocument;
+                    if (((selectedItem != null) ? selectedItem.Document : null) != null)
+                    {
+                        currentSelectedDocument = workspace.CurrentSolution.GetDocumentByFilePath(selectedItem.Document.FullName);
+                    }
+                    else
+                    {
+                        if (selectedItem == null)
+                        {
+                            "致命异常-无法获得当前选择的解决方案：请重新打开这个解决方案，然后重试本工具。(官网：52abp.com)".ErrorMsg();
+                            return;
+                        }
+                        string file = selectedItem.Name;
+                        string projectName = selectedItem.ContainingProject.Name;
+                        List<Project> allProjects = workspace.CurrentSolution.Projects.ToList<Project>();
+                        List<Project> tmpProjects = (from o in allProjects
+                                                     where o.Name.StartsWith(projectName + "(net")
+                                                     select o).ToList<Project>();
+                        List<Document> docs;
+                        if (allProjects.Exists((Project p) => p.Name.Contains(".Shared")))
+                        {
+                            docs = (from d in (from p in allProjects
+                                               where p.Name.Contains(projectName) && !p.Name.Contains(".Shared")
+                                               select p).FirstOrDefault<Project>().Documents
+                                    where d.Name == file
+                                    select d).ToList<Document>();
+                        }
+                        else if (tmpProjects.Count > 1)
+                        {
+                            docs = (from d in tmpProjects.FirstOrDefault<Project>().Documents
+                                    where d.Name == file
+                                    select d).ToList<Document>();
+                        }
+                        else
+                        {
+                            docs = (from d in (from p in allProjects
+                                               where p.Name == projectName
+                                               select p).SelectMany((Project p) => p.Documents)
+                                    where d.Name == file
+                                    select d).ToList<Document>();
+                        }
+                        if (docs.Count == 0)
+                        {
+                            "致命异常-无法获得当前选择的解决方案。请尝试重新打开解决方案，然后再使用本工具(官网：52abp.com)".ErrorMsg();
+                            return;
+                        }
+                        if (docs.Count > 1)
+                        {
+                            "当前解决方案中，有多个同名的类文件，请处理之后再使用本工具，比如删除一个^_^(官网：52abp.com)。".ErrorMsg();
+                            return;
+                        }
+                        currentSelectedDocument = docs.FirstOrDefault<Document>();
+                    }
+                    SolutionInfoModel solutionInfo = SolutionInfoCreator.Create(currentSelectedDocument);
+                    string path = Path.Combine(solutionInfo.SolutionPath, "solutionInfo.json");
+                    string solutionInfoJson = JsonConvert.SerializeObject(solutionInfo);
+                    path.CreateFile(solutionInfoJson);
+                    ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                    string exePath = Path.Combine(Path.GetDirectoryName(typeof(CommonConsts).Assembly.Location), "YoyoAbpCodePowerProject.WPF.exe");
+                    processStartInfo.FileName = exePath;
+                    processStartInfo.Arguments = " \"" + solutionInfo.SolutionPath + "\"";
+                    Process.Start(processStartInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    "发生了一个异常。请复制 c/p 堆栈信息，然后打开项目网站(https://github.com/52ABP/52ABP.CodeGenerator),补充问题和增加简短描述。".ErrorMsg();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("<html><head></head><body><a hrf='https://github.com/52ABP/52ABP.CodeGenerator/issues/new'>baidu</a><br>");
+                    sb.Append(ex.ToString());
+                    sb.Append("</body></html>");
+                    string tmpFile = Path.GetTempFileName();
+                    File.WriteAllText(tmpFile, sb.ToString());
+                    VsShellUtilities.OpenBrowser("file:///" + tmpFile);
+                }
+                catch (Exception)
+                {
+                    "发生了一个异常。 无法将堆栈跟踪写入临时目录.".ErrorMsg();
+                }
+            }
+        }
+
+        private ProjectItem GetSelectedSolutionExplorerItem()
+        {
+            DTE2 ide = ServiceProvider.GetServiceAsync(typeof(DTE)).Result as DTE2;
+
+            object[] items = ide.ToolWindows.SolutionExplorer.SelectedItems as object[];
+            if (items.Length != 1)
+            {
+                return null;
+            }
+            UIHierarchyItem hierarchyItem = items[0] as UIHierarchyItem;
+            if (hierarchyItem == null)
+            {
+                return null;
+            }
+            ProjectItem projectItem = ide.Solution.FindProjectItem(hierarchyItem.Name);
+            if (projectItem == null)
+            {
+                return null;
+            }
+            return projectItem;
+        }
+    }
+}
---
+++ LGVSIX/SolutionInfoCreator.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LG.AbpDtoGenerator;
+using LG.AbpDtoGenerator.CodeAnalysis;
+using LG.AbpDtoGenerator.Enums;
+using LG.AbpDtoGenerator.GeneratorModels;
+using LG.AbpDtoGenerator.Models;
+using Microsoft.CodeAnalysis;
+
+namespace DtoGenerator.Vsix
+{
+    public static class SolutionInfoCreator
+    {
+        public static SolutionInfoModel Create(Document currentSelectedDocument)
+        {
+            bool isAbpZero = false;
+            Solution solution = currentSelectedDocument.Project.Solution;
+            currentSelectedDocument.GetPossibleProjects();
+            List<Project> allProject = solution.Projects.ToList<Project>();
+            string currentProjectName = currentSelectedDocument.Project.Name;
+            if (currentProjectName.Contains("("))
+            {
+                currentProjectName = currentProjectName.Substring(0, currentProjectName.IndexOf("("));
+            }
+            string projectStartName = currentSelectedDocument.Project.Name.Substring(0, currentProjectName.LastIndexOf("."));
+            if (allProject.Exists((Project p) => p.Name.Contains(".Shared")))
+            {
+                isAbpZero = true;
+            }
+            Project application_SharedProject = null;
+            Project core_SharedProject = null;
+            Project applicationProject;
+            Project coreProject;
+            Project efProject;
+            Project mvcProject;
+            Project portalProject;
+            Project hostProject;
+            Project webCoreProject;
+            if (isAbpZero)
+            {
+                applicationProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Application") && !item.Name.Contains(".Shared"));
+                application_SharedProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Application.Shared"));
+                coreProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Core") && !item.Name.Contains(".Shared"));
+                core_SharedProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Core.Shared"));
+                efProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".EntityFrameworkCore") && !item.Name.Contains(".Shared"));
+                mvcProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Web.Mvc"));
+                portalProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Web.Portal"));
+                hostProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Web.Host"));
+                webCoreProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Web.Core"));
+            }
+            else
+            {
+                applicationProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Application"));
+                coreProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Core"));
+                efProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".EntityFrameworkCore"));
+                mvcProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Web.Mvc"));
+                portalProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Web.Portal"));
+                hostProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Web.Host"));
+                webCoreProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Web.Core"));
+                if (efProject == null)
+                {
+                    "生成器只支持.net core/standard版本！不支持.net framework版本！".ErrorMsg();
+                    return null;
+                }
+            }
+            Project xUnitTestsProject = allProject.Find((Project item) => item.Name.StartsWith(projectStartName + ".Tests"));
+            if (applicationProject == null || coreProject == null || efProject == null)
+            {
+                "当前项目不是标准的ABP框架结构！请检查框架是否正确！".ErrorMsg();
+                return null;
+            }
+            string solutionNamespace = string.Empty;
+            string companyNamespace = string.Empty;
+            ProjectPathInfo application = new ProjectPathInfo
+            {
+                PType = ProjectType.Application,
+                BasePath = applicationProject.GetProjectPath(),
+                ProjectName = applicationProject.Name
+            };
+            ProjectPathInfo core = new ProjectPathInfo
+            {
+                PType = ProjectType.Core,
+                BasePath = coreProject.GetProjectPath(),
+                ProjectName = coreProject.Name
+            };
+            ProjectPathInfo ef = new ProjectPathInfo
+            {
+                PType = ProjectType.Ef,
+                BasePath = efProject.GetProjectPath(),
+                ProjectName = efProject.Name
+            };
+            string[] dtoSplitStrings = currentProjectName.Split(new char[]
+            {
+                '.'
+            });
+            if (dtoSplitStrings.Length < 3)
+            {
+                solutionNamespace = dtoSplitStrings[0];
+                companyNamespace = string.Empty;
+            }
+            else
+            {
+                if (dtoSplitStrings.Length <= 2)
+                {
+                    throw new Exception("解决方案项目的多层架构不符合52ABP规范，建议名称为Company.Project的形式。");
+                }
+                solutionNamespace = dtoSplitStrings[1];
+                companyNamespace = dtoSplitStrings[0] + ".";
+            }
+            SolutionInfoModel solutionInfoModel = new SolutionInfoModel();
+            solutionInfoModel.IsAbpZero = isAbpZero;
+            solutionInfoModel.Application = application;
+            solutionInfoModel.Core = core;
+            solutionInfoModel.EF = ef;
+            solutionInfoModel.SolutionNamespace = solutionNamespace;
+            solutionInfoModel.CompanyNamespace = companyNamespace;
+            solutionInfoModel.CurrentProjectName = currentProjectName;
+            solutionInfoModel.CurrentSelectFilePath = currentSelectedDocument.FilePath;
+            solutionInfoModel.SolutionPath = Path.GetDirectoryName(solution.FilePath);
+            if (hostProject != null)
+            {
+                ProjectPathInfo host = new ProjectPathInfo
+                {
+                    PType = ProjectType.Host,
+                    BasePath = hostProject.GetProjectPath(),
+                    ProjectName = hostProject.Name
+                };
+                solutionInfoModel.Host = host;
+            }
+            if (webCoreProject != null)
+            {
+                ProjectPathInfo webCore = new ProjectPathInfo
+                {
+                    PType = ProjectType.WebCore,
+                    BasePath = webCoreProject.GetProjectPath(),
+                    ProjectName = webCoreProject.Name
+                };
+                solutionInfoModel.WebCore = webCore;
+            }
+            if (mvcProject != null)
+            {
+                ProjectPathInfo mvc = new ProjectPathInfo
+                {
+                    PType = ProjectType.Mvc,
+                    BasePath = mvcProject.GetProjectPath(),
+                    ProjectName = mvcProject.Name
+                };
+                solutionInfoModel.MVC = mvc;
+            }
+            if (xUnitTestsProject != null)
+            {
+                ProjectPathInfo xUnitTests = new ProjectPathInfo
+                {
+                    PType = ProjectType.XUnitTests,
+                    BasePath = xUnitTestsProject.GetProjectPath(),
+                    ProjectName = xUnitTestsProject.Name
+                };
+                solutionInfoModel.Tests = xUnitTests;
+            }
+            if (portalProject != null)
+            {
+                ProjectPathInfo portal = new ProjectPathInfo
+                {
+                    PType = ProjectType.Portal,
+                    BasePath = portalProject.GetProjectPath(),
+                    ProjectName = portalProject.Name
+                };
+                solutionInfoModel.Portal = portal;
+            }
+            if (isAbpZero)
+            {
+                ProjectPathInfo application_Shared = new ProjectPathInfo
+                {
+                    PType = ProjectType.Application_Shared,
+                    BasePath = application_SharedProject.GetProjectPath(),
+                    ProjectName = application_SharedProject.Name
+                };
+                solutionInfoModel.Application_Shared = application_Shared;
+                ProjectPathInfo projectPathInfo = new ProjectPathInfo();
+                projectPathInfo.PType = ProjectType.Core_Shared;
+                projectPathInfo.BasePath = core_SharedProject.GetProjectPath();
+                projectPathInfo.ProjectName = core_SharedProject.Name;
+                solutionInfoModel.Core_Shared = application_Shared;
+            }
+            return solutionInfoModel;
+        }
+    }
+}
